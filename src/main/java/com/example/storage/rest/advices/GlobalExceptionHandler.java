df47aab471c12898854cb50b8f813aac624f95cd
Alex Orozov <alexorozov@gmail.com>
2023-07-19 02:15:03 +0300

    Trying to commit from PC.

--- src/main/java/com/example/storage/rest/advices/GlobalExceptionHandler.java
+++ src/main/java/com/example/storage/rest/advices/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.example.storage.rest.advices;
 
+import com.example.storage.core.exceptions.ItemNotFoundInRepositoryException;
 import com.example.storage.core.exceptions.NotEnoughQuantityOfSelectedItemException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,5 +19,10 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(NotEnoughQuantityOfSelectedItemException.class)
     public ResponseEntity<String> handleMethodArgumentNotValidException(NotEnoughQuantityOfSelectedItemException e){
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("That was an invalid request input! " + e.getMessage());
+    }
+
+    @ExceptionHandler(ItemNotFoundInRepositoryException.class)
+    public ResponseEntity<String> handleItemNotFoundInRepositoryException(ItemNotFoundInRepositoryException e){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("No item has been found in the repository! " + e.getMessage());
     }
 }
