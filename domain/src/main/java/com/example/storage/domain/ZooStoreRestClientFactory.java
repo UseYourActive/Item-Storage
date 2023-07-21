36ad467864124cf1c4726aea8f9ab49f91785e4d
Alex Orozov <alexorozov@gmail.com>
2023-07-21 13:47:14 +0300

    Flatting POMs

---
+++ domain/src/main/java/com/example/storage/domain/ZooStoreRestClientFactory.java
@@ -0,0 +1,29 @@
+package com.example.storage.domain;
+
+import com.example.zoostore.restexport.ZooStoreRestClient;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import feign.Feign;
+import feign.jackson.JacksonDecoder;
+import feign.jackson.JacksonEncoder;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@RequiredArgsConstructor
+@Configuration
+public class ZooStoreRestClientFactory {
+    private final ObjectMapper objectMapper;
+
+    @Bean
+    ZooStoreRestClient getRestExportClient() {
+        return Feign.builder()
+                .encoder(new JacksonEncoder(objectMapper))
+                .decoder(new JacksonDecoder(objectMapper))
+                .target(ZooStoreRestClient.class, "http://localhost:8081");
+    }
+
+    @Bean
+    public ObjectMapper objectMapper(){
+        return new ObjectMapper();
+    }
+}
